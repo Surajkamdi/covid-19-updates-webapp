a14b5d0cbaace197c31b18375251418bcfe609f9
Suraj Kamdi <suraj.kamdi@perficient.com>
2020-03-24 10:57:15 +0530

    Updates

---
+++ precache-manifest.789d6a4a1a7352e8ee68a5fe2b555bd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5223c8173bb0d4230a463f2fb1d108de",
+    "url": "/covid-19-updates-webapp/index.html"
+  },
+  {
+    "revision": "6a866752cf0b94ab0f68",
+    "url": "/covid-19-updates-webapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d1f0484baeae6774919",
+    "url": "/covid-19-updates-webapp/static/js/2.6878ada9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19-updates-webapp/static/js/2.6878ada9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a866752cf0b94ab0f68",
+    "url": "/covid-19-updates-webapp/static/js/main.5b0879c8.chunk.js"
+  },
+  {
+    "revision": "22cdfc51814b4a183c9e",
+    "url": "/covid-19-updates-webapp/static/js/runtime-main.d9431752.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-updates-webapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
