add810494d506dc3c79d2b45fa3b99b6cdfcd99c
Suraj Kamdi <suraj.kamdi@perficient.com>
2020-03-31 13:13:10 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19-updates-webapp/precache-manifest.831ffc3489fe8c82cbc4f139e3bcaac0.js"
+  "/covid-19-updates-webapp/precache-manifest.263139b1803a1f6571760219ef4b35ae.js"
 );
 
 self.addEventListener('message', (event) => {
